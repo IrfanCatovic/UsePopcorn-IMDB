461361fd9303bccf58099cb7aee62c0ed45461fc
IrfanCatovic <catovicc84@gmail.com>
2023-11-29 20:38:25 +0100

    Added rating function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+// import "./index.css";
+// import App from "./App";
+import StarRaiting from "./StarRaiting";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App/> */}
+    <StarRaiting maxRating={10} />
+    <StarRaiting />
   </React.StrictMode>
 );
