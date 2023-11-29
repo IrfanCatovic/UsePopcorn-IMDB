d476cceb95df45a05ce93368d1fb1cea85cb00c2
IrfanCatovic <catovicc84@gmail.com>
2023-11-29 02:44:21 +0100

    prop drill fixed

--- src/App.js
+++ src/App.js
@@ -55,18 +55,26 @@ export default function App() {
 
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+        <Search />
+        <NumResults movies={movies} /> 
+      </NavBar>
+      
+      <Main >
+        <ListBox >
+        <MovieList movies={movies}/>
+           </ListBox>
+        <WatchedBox  />
+      </Main>
     </>
   );
 }
 
-function NavBar({ movies }) {
+function NavBar({ children }) {
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search />
-      <NumResults movies={movies} />
+        {children}
     </nav>
   );
 }
@@ -101,16 +109,15 @@ function NumResults({ movies }) {
   );
 }
 
-function Main({ movies }) {
+function Main({ children }) {
   return (
     <main className="main">
-      <ListBox />
-      <WatchedBox movies={movies} />
+      {children}
     </main>
   );
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
   return (
     <div className="box">
@@ -120,7 +127,7 @@ function ListBox({ movies }) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children }
     </div>
   );
 }
