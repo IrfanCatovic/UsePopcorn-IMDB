a6078c52fd47778d7ca2d8ad7c2a9ad871883e49
IrfanCatovic <catovicc84@gmail.com>
2023-12-05 13:45:39 +0100

    First part of popcorn app finished

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRaiting from "./StarRaiting";
+import "./index.css";
 import App from "./App";
+import StarRaiting from "./StarRaiting";
 
 function Test() {
   const [movieRating, setMovieRating] = useState(0);
@@ -19,7 +18,7 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App/> */}
+    <App />
     <StarRaiting
       maxRating={5}
       messages={["Bad", "Not Bad", "Good", "Great", "Amazing"]}
