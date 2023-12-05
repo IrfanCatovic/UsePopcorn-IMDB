bc89484818cb5eda8ba49c2a77e6d8bc1b8304de
IrfanCatovic <catovicc84@gmail.com>
2023-12-05 10:22:41 +0100

    Api part 3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRaiting from "./StarRaiting";
 import App from "./App";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRaiting color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,5 +25,7 @@ root.render(
       messages={["Bad", "Not Bad", "Good", "Great", "Amazing"]}
     />
     <StarRaiting size={24} color="red" className="test" defaultRating={3} />
+
+    <Test />
   </React.StrictMode>
 );
