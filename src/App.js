11e459fa162647fe78dd281c4c452c2a54713cd4
IrfanCatovic <catovicc84@gmail.com>
2023-12-20 11:17:33 +0100

    See details of selected movies from imdb

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import StarRaiting from "./StarRaiting";
 
 const tempMovieData = [
   {
@@ -168,7 +169,7 @@ export default function App() {
 }
 
 function Loader() {
-  <p className="loader"> Loading... </p>;
+  return <p className="loader">Loading...</p>;
 }
 
 function NavBar({ children }) {
@@ -284,6 +285,8 @@ function Movie({ movie, onSelectMovie }) {
 
 function MovieDetails({ selectedId, onCloseMovie }) {
   const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
   const {
     Title: title,
     Year: year,
@@ -302,11 +305,13 @@ function MovieDetails({ selectedId, onCloseMovie }) {
   useEffect(
     function () {
       async function getMovieDetails() {
+        setIsLoading(true);
         const res = await fetch(
           `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
+        setIsLoading(false);
       }
       getMovieDetails();
     },
@@ -315,24 +320,40 @@ function MovieDetails({ selectedId, onCloseMovie }) {
 
   return (
     <div className="details">
-      <header>
-        <button className="btn-back" onClick={onCloseMovie}>
-          &larr;
-        </button>
-        <img src={poster} alt={`Poster of ${movie} movie`} />
-        <div className="details-overview">
-          <h2>{title}</h2>
-          <p>
-            {released} &bull; {runtime}
-          </p>
-          <p>{genre}</p>
-          <p>
-            <span>⭐️</span>
-            {imdbRating} IMDb rating
-          </p>
-        </div>
-      </header>
-      {selectedId}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <header>
+            <button className="btn-back" onClick={onCloseMovie}>
+              &larr;
+            </button>
+            <img src={poster} alt={`Poster of ${movie} movie`} />
+            <div className="details-overview">
+              <h2>{title}</h2>
+              <p>
+                {released} &bull; {runtime}
+              </p>
+              <p>{genre}</p>
+              <p>
+                <span>⭐️</span>
+                {imdbRating} IMDb rating
+              </p>
+            </div>
+          </header>
+
+          <section>
+            <div className="raiting">
+              <StarRaiting maxRating={10} size={24} />
+            </div>
+            <p>
+              <em>{plot}</em>
+              <p>Starring: {actors}</p>
+              <p>Directed by {director}</p>
+            </p>
+          </section>
+        </>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,27 +4,27 @@ import "./index.css";
 import App from "./App";
 import StarRaiting from "./StarRaiting";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRaiting color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRaiting color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <StarRaiting
+    {/* <StarRaiting
       maxRating={5}
       messages={["Bad", "Not Bad", "Good", "Great", "Amazing"]}
     />
-    <StarRaiting size={24} color="red" className="test" defaultRating={3} />
+    <StarRaiting size={24} color="red" className="test" defaultRating={3} /> */}
 
-    <Test />
+    {/* <Test /> */}
   </React.StrictMode>
 );
